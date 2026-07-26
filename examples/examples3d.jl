719ff53531c66da41b7941af8d8d921ca039ccaf
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2026-07-26 23:41:21 +0200

    fix cairomakie handling

--- examples/examples3d.jl
+++ examples/examples3d.jl
@@ -153,28 +153,25 @@ end
 # Plot generation
 using GridVisualize
 function generateplots(picdir; Plotter = nothing)
-    return if isdefined(Plotter, :gcf)
-        size = (300, 300)
+    if isdefined(Plotter, :Makie)
+        size = (600, 250)
 
-        Plotter.clf()
-        gridplot(tetrahedralization_of_cube(); Plotter, size, zplane = 0.5)
-        Plotter.savefig(joinpath(picdir, "tetrahedralization_of_cube.png"))
+        p = gridplot(tetrahedralization_of_cube(); Plotter, size, zplane = 0.5, linewidth = 0.1)
+        Plotter.save(joinpath(picdir, "tetrahedralization_of_cube.png"), p)
 
-        Plotter.clf()
-        gridplot(tet_cube_with_primitives(); Plotter, size, zplane = 5, azim = 47, elev = 80, interior = false)
-        Plotter.savefig(joinpath(picdir, "tet_cube_with_primitives.png"))
+        p = gridplot(tet_cube_with_primitives(); Plotter, size, zplane = 5, azim = 47, elev = 80, interior = false, linewidth = 0.1)
+        Plotter.save(joinpath(picdir, "tet_cube_with_primitives.png"), p)
 
-        Plotter.clf()
-        gridplot(glue_3d(); Plotter, size, azim = 0, elev = 15, xplanes = [5])
-        Plotter.savefig(joinpath(picdir, "glue_3d.png"))
+        p = gridplot(glue_3d(); Plotter, size, azim = 0, elev = 15, xplanes = [5], linewidth = 0.1)
+        Plotter.save(joinpath(picdir, "glue_3d.png"), p)
 
-        Plotter.clf()
-        gridplot(remesh_3d(); Plotter, size, zplanes = [0.5])
-        Plotter.savefig(joinpath(picdir, "remesh_3d.png"))
+        p = gridplot(remesh_3d(); Plotter, size, zplanes = [0.5], linewidth = 0.1)
+        Plotter.save(joinpath(picdir, "remesh_3d.png"), p)
 
-        Plotter.clf()
-        gridplot(stl_3d(); Plotter, size, xplanes = [5])
-        Plotter.savefig(joinpath(picdir, "stl_3d.png"))
+        p = gridplot(stl_3d(); Plotter, size, xplanes = [5], linewidth = 0.1)
+        Plotter.save(joinpath(picdir, "stl_3d.png"), p)
+
 
     end
+    return
 end
